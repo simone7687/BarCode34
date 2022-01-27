0cbbd1287fb9ac9c42a9bc17c78719ba4f001694
Anonimus <NoAccount@anonimus.com>
2022-01-27 21:03:28 +0100

    Main

--- app/src/main/java/com/example/barcode/MainActivity.kt
+++ app/src/main/java/com/example/barcode/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.example.barcode
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (isCameraPermissionGranted()) {
+            // startCamera
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                PERMISSION_CAMERA_REQUEST
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_CAMERA_REQUEST) {
+            if (isCameraPermissionGranted()) {
+                // start camera
+            } else {
+                Log.e(TAG, "no camera permission")
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun isCameraPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            baseContext,
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+        private const val PERMISSION_CAMERA_REQUEST = 1
     }
 }
