9cb24a2506f76008ab0125add65697cb0508e1c4
Anonimus <NoAccount@anonimus.com>
2022-01-27 22:01:17 +0100

    Test

--- app/src/main/java/com/example/barcode/MainActivity.kt
+++ app/src/main/java/com/example/barcode/MainActivity.kt
@@ -5,16 +5,41 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat
+import com.google.mlkit.vision.barcode.BarcodeScanning
+
+import com.google.mlkit.vision.barcode.BarcodeScanner
+
+import com.google.mlkit.vision.barcode.common.Barcode
+
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+
+
+
 
 class MainActivity : AppCompatActivity() {
+    var options = BarcodeScannerOptions.Builder()
+        .setBarcodeFormats(
+            Barcode.FORMAT_QR_CODE,
+            Barcode.FORMAT_CODE_39
+        ).build()
+
+    // This is how you can get an instance of BarcodeScanner
+    var barcodeScanner = BarcodeScanning.getClient(options)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         if (isCameraPermissionGranted()) {
             // startCamera
+            startCamera()
         } else {
             ActivityCompat.requestPermissions(
                 this,
@@ -23,6 +48,40 @@ class MainActivity : AppCompatActivity() {
             )
         }
     }
+
+    private fun startCamera() {
+        // Create an instance of the ProcessCameraProvider,
+        // which will be used to bind the use cases to a lifecycle owner.
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        // Add a listener to the cameraProviderFuture.
+        // The first argument is a Runnable, which will be where the magic actually happens.
+        // The second argument (way down below) is an Executor that runs on the main thread.
+        cameraProviderFuture.addListener({
+            // Add a ProcessCameraProvider, which binds the lifecycle of your camera to
+            // the LifecycleOwner within the application's life.
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            // Initialize the Preview object, get a surface provider from your PreviewView,
+            // and set it on the preview instance.
+            val preview = Preview.Builder().build().also {
+                it.setSurfaceProvider(
+                    fragment_scan_barcode_preview_view.surfaceProvider
+                )
+            }
+
+            // Select back camera
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try {
+                // Unbind any bound use cases before rebinding
+                cameraProvider.unbindAll()
+                // Bind use cases to lifecycleOwner
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            } catch (e: Exception) {
+                Log.e("PreviewUseCase", "Binding failed! :(", e)
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
